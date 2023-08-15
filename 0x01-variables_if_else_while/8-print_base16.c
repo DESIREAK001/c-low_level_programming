dfef963e423f00fdae335eb8f4cf90d343c6e333
DESIREAK001 <desirengonzikabagyo@gmail.com>
2023-08-15 08:30:28 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+int x;
+char y;
+for (x = 0; x <= 9; x++)
+putchar(x + '0');
+
+for (y = 'a'; y <= 'f'; y++)
+putchar(y);
+putchar('\n');
+}
