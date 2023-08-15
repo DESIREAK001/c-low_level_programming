e108f9719449d0643670e1bbadd8ba311801bc67
DESIREAK001 <desirengonzikabagyo@gmail.com>
2023-08-15 07:24:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+int x;
+
+
+for (x = 0; x <= 9; x++)
+putchar(x);
+putchar('\n');
+
+return (0);
+}
