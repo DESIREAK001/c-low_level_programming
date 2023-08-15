761e510d5125c3531e8b55f1cd7aaff8dbefe051
DESIREAK001 <desirengonzikabagyo@gmail.com>
2023-08-15 08:03:07 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+char x;
+
+for (x = 'z'; x >= 'a'; x--)
+putchar(x);
+putchar('\n');
+
+return (0);
+}
